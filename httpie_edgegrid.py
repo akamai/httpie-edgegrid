d034e0327cc958e034b0082d3d6c3d42c676842d
Hunter, Kirsten <khunter@akamai.com>
2015-06-26 19:04:09 -0700

    Making the system use .edgerc correctly

--- httpie_edgegrid.py
+++ httpie_edgegrid.py
@@ -4,19 +4,19 @@ Edgegrid auth plugin for HTTPie.
 """
 from httpie.plugins import AuthPlugin
 from akamai.edgegrid import EdgeGridAuth
-import ConfigParser
+import ConfigParser, os
 __version__ = '1.0.0'
 __author__ = 'Kirsten Hunter'
 __licence__ = 'Apache 2.0'
 
-hostname = ''
-
 class MyEdgeGridAuth(EdgeGridAuth):
+
 
     def __call__(self, r):
         r = super(MyEdgeGridAuth, self).__call__(r)
-	print "Setting up the call now %s" % hostname
-#        r.headers['Host'] = hostname
+        #r.headers['Host'] = os.environ['HOST'] 
+	r.url = r.url.replace("http","https")
+	r.url = r.url.replace("localhost/",os.environ['HOST'])
         return r
 
 
@@ -29,19 +29,18 @@ class EdgeGridPlugin(AuthPlugin):
     def get_auth(self,username,password):
     	arguments = {}
     	config = ConfigParser.ConfigParser()
-    	config.readfp(open(".edgerc"))
+	home = os.environ['HOME']	
+	print "signing"
+    	config.readfp(open("%s/.edgerc" % home))
     	if not config.has_section(username):
                         err_msg = "ERROR: No section named %s was found in your .edgerc file\n" % (configuration, arguments["config_file"])
                         err_msg += "ERROR: Please generate credentials for the script functionality\n"
                         err_msg += "ERROR: and run 'python gen_edgerc.py %s' to generate the credential file\n" % configuration
                         sys.exit( err_msg )
     	for key, value in config.items(username):
-		print key
-		print value
 		arguments[key] = value	
-		if key == 'host':
-			hostname = value
-
+		if key == "host":
+			os.environ['HOST'] = value
         return MyEdgeGridAuth(
             client_token=arguments["client_token"],
             client_secret=arguments["client_secret"],
