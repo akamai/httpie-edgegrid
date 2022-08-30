c212244d88d2374c5f48c4d79cda2ffed7f6fc95
Tatiana Slonimskaia <tslonims@akamai.com>
2022-08-30 09:31:39 +0000

    DXE-1521 Added release entry

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     },
     install_requires=[
         'httpie == 3.2.1',
-        'edgegrid-python == 1.2.1',
+        'edgegrid-python == 1.3.0',
         'pyOpenSSL == 22.0.0'
     ],
 
